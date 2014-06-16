75aa6aee4a54bea981efa127e4c6216260416c19
aelred <aelred717@gmail.com>
2014-06-16 19:15:44 +0100

    Remove parameters for several show filters which were not very useful and cluttered the config files

--- nab/plugins/shows/show_filter.py
+++ nab/plugins/shows/show_filter.py
@@ -17,9 +17,8 @@ Exclude.register("exclude")
 
 class Specials(ShowFilter):
 
-    def __init__(self, include=True, exclude=None):
-        self.include = include or []
-        self.exclude = exclude or []
+    def __init__(self, *exclude):
+        self.exclude = exclude
 
     def filter_episode(self, episode):
         def func(ep, keywords, inverse=False):
@@ -37,7 +36,7 @@ class Specials(ShowFilter):
         if episode.season.num != 0:
             return True
 
-        if not func(episode, self.include):
+        if not func(episode, False):
             return False
 
         if not func(episode, self.exclude, True):
@@ -47,35 +46,23 @@ Specials.register("specials")
 
 
 class Watched(ShowFilter):
-
-    def __init__(self, include):
-        self.include = include
-
     def filter_episode(self, episode):
-        return self.include or not episode.watched
+        return not episode.watched
 Watched.register("watched")
 
 
 class Owned(ShowFilter):
-
-    def __init__(self, include):
-        self.include = include
-
     def filter_episode(self, episode):
-        return self.include or not episode.owned
+        return not episode.owned
 Owned.register("owned")
 
 
 class Next(ShowFilter):
 
-    def __init__(self, include, include_first=True):
-        self.include = include
+    def __init__(self, include_first=True):
         self.include_first = include_first
 
     def filter_episode(self, episode):
-        if not self.include:
-            return True
-
         if (episode.num == 1 and episode.season.num == 1
            and not self.include_first):
             return False
@@ -83,5 +70,5 @@ class Next(ShowFilter):
         if not episode.previous:
             return True
 
-        return not self.include or episode.previous.watched
+        return episode.previous.watched
 Next.register("next")
