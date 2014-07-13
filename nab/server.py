725e1cc935bd6bd8d65a16d10ab47e0bccf31526
aelred <aelred717@gmail.com>
2014-07-13 16:07:49 +0100

    Add support for editing paths on config file

--- nab/server.py
+++ nab/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response
 import yaml
+import copy
 
 import nab.config
 
@@ -19,10 +20,31 @@ def log():
 
 
 @app.route('/config', methods=['GET', 'POST'])
-def config():
-    if request.method == 'POST':
-        nab.config.change_config(yaml.safe_load(request.data))
+def config_all():
+    return config([])
 
-    response = make_response(yaml.safe_dump(nab.config.config))
+
+@app.route('/config/<path:path>', methods=['GET', 'POST'])
+def config_path(path=''):
+    return config(path.split('/'))
+
+
+def config(path):
+    # navigate provided path along config file
+    conf_copy = copy.deepcopy(nab.config.config)
+    conf_sub = conf_copy
+    # navigate to second-from-last index, to allow referencing later
+    for p in path[:-1]:
+        conf_sub = conf_sub[p]
+
+    response = make_response(yaml.safe_dump(conf_sub[path[-1]]))
     response.headers['content-type'] = 'text/yaml'
+
+    if request.method == 'POST':
+        # replace config path with given data
+        # index used so change is made to conf_copy
+        conf_sub[path[-1]] = yaml.safe_load(request.data)
+        # update config
+        nab.config.change_config(conf_copy)
+
     return response
