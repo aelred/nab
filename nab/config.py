c4b82bf7f696de9c2374923fde166e0ed8cd8b6e
aelred <aelred717@gmail.com>
2014-07-13 15:02:18 +0100

    Add log events for config changes and support to change config file

--- nab/config.py
+++ nab/config.py
@@ -7,12 +7,17 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 from nab.scheduler import scheduler, tasks
+from nab import log
+
+_log = log.log.getChild("config")
 
 
 def _load_config():
     if not os.path.exists("config.yaml"):
+        _log.info("Creating default config file")
         copyfile("config_default.yaml", "config.yaml")
 
+    _log.info("Loading config and accounts files")
     c = yaml.load(file("config.yaml", "r"))
     a = yaml.load(file("accounts.yaml", "a+"))
 
@@ -29,6 +34,7 @@ def _load_config():
     dirs = [s["downloads"], s["completed"]] + s["videos"]
     for d in dirs:
         if not os.path.exists(d):
+            _log.info("Creating directory %s" % d)
             os.makedirs(d)
 
     return c, a
@@ -36,9 +42,15 @@ config, accounts = _load_config()
 
 
 def reload_config():
+    _log.info('Reloading config and accounts files')
     global config, accounts
     config, accounts = _load_config()
 tasks["load_config"] = reload_config
+
+
+def change_config(new_config):
+    _log.info('Changing config file')
+    yaml.safe_dump(new_config, file('config.yaml', 'w'))
 
 
 def init():
@@ -50,8 +62,12 @@ def init():
 
 class ConfigWatcher(FileSystemEventHandler):
     def on_modified(self, event):
-        if event.src_path == os.path.join(os.getcwd(), "config.yaml"):
-            scheduler.add_asap("load_config")
+        if event.src_path == os.path.join(os.getcwd(), 'config.yaml'):
+            _log.info('Change detected in config.yaml, scheduling reload')
+            scheduler.add_asap('load_config')
+        if event.src_path == os.path.join(os.getcwd(), 'accounts.yaml'):
+            _log.info('Change detected in accounts.yaml, scheduling reload')
+            scheduler.add_asap('load_config')
 
 
 def _load_options():
