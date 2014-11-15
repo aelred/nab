8fd0d0499d38529a5f74d5a867cfeaa37a5d71bc
Felix Chapman <aelred717@gmail.com>
2014-11-15 01:17:53 +0000

    Add basic setup info including requirements to setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,16 @@ except WindowsError:
     pass
 
 setup(
+    name='nab',
+    version='0.1',
+    description='Automatic TV show download tool',
+    author='Felix Chapman',
+    packages=['nab', 'nab.plugins', 'nab.plugins.databases',
+              'nab.plugins.downloaders', 'nab.plugins.filesources',
+              'nab.plugins.shows'],
+    requires=['appdirs', 'requests', 'tvdb_api', 'filecache', 'plex',
+              'watchdog', 'flask'],
+
     options={'py2exe': {
         'bundle_files': 1,
         'compressed': True,
@@ -34,5 +44,9 @@ setup(
     )
 
 # rename main file to ensemble.exe
-shutil.move(os.path.join('dist', '__main__.exe'),
-            os.path.join('dist', 'nab.exe'))
+try:
+    shutil.move(os.path.join('dist', '__main__.exe'),
+                os.path.join('dist', 'nab.exe'))
+except IOError:
+    # if not an executable, skip
+    pass
