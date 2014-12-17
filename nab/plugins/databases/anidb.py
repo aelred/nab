5edd0ddccb29e2ea8e978faa48f817e38487ef50
Felix Chapman <aelred717@gmail.com>
2014-12-17 15:18:00 +0000

    Improve formatting and documentation in anidb

--- nab/plugins/databases/anidb.py
+++ nab/plugins/databases/anidb.py
@@ -1,3 +1,4 @@
+""" Plugin for accessing information from anidb. """
 from collections import namedtuple, defaultdict
 import requests
 import xml.etree.ElementTree as ET
@@ -5,7 +6,6 @@ import urllib
 import gzip
 import time
 from filecache import filecache
-from pprint import pprint
 from lxml import html
 from itertools import chain, product
 from munkres import Munkres
@@ -28,9 +28,10 @@ defparams = {
 langs = ["en", "x-jat"]
 ns = "{http://www.w3.org/XML/1998/namespace}"
 
-# fetch names database only once every two days max
+
 @filecache(7 * 24 * 60 * 60)
-def get_db():
+def _get_db():
+    # fetch names database only once every two days max
     urllib.urlretrieve("http://anidb.net/api/anime-titles.xml.gz",
                        titles_file)
 
@@ -40,7 +41,7 @@ dbi = {}
 dbe = namedtuple("dbe", ["id", "title", "titles"])
 
 
-def load_db():
+def _load_db():
     def main_title(show):
         for t in show:
             if t.get("type") == "official" and t.get("%slang" % ns) == "en":
@@ -66,8 +67,8 @@ _init_flag = False
 
 
 def _init():
-    get_db()
-    load_db()
+    _get_db()
+    _load_db()
     global _init_flag
     _init_flag = True
 
@@ -78,7 +79,7 @@ _get_interval = 2.0
 
 # only request anidb data once a week max
 @filecache(7 * 24 * 60 * 60)
-def get(*args, **kwargs):
+def _get(*args, **kwargs):
     global _last_get
 
     t = time.clock()
@@ -94,23 +95,23 @@ def get(*args, **kwargs):
     return r
 
 
-def entries(title):
+def _entries(title):
     return dbt[format_title(title)]
 
 
-def info(entry):
+def _info(entry):
     Anidb.log.debug("Getting info for %s" % entry.title)
     params = dict(defparams, **{"request": "anime", "aid": entry.id})
-    r = get(url, params=params)
+    r = _get(url, params=params)
     Anidb.log.debug("Found info for %s" % entry.title)
     return html.fromstring(r.text.encode("ascii", "ignore"))
 
 
-def type(entry):
-    return info(entry).xpath('//type/text()')[0]
+def _type(entry):
+    return _info(entry).xpath('//type/text()')[0]
 
 
-def get_time(time_str):
+def _get_time(time_str):
     try:
         return time.mktime(time.strptime(time_str, "%Y-%m-%d"))
     except ValueError:
@@ -125,27 +126,27 @@ def get_time(time_str):
         return 0
 
 
-def startdate(entry):
-    date = info(entry).xpath(".//startdate/text()")[0]
-    return get_time(date)
+def _startdate(entry):
+    date = _info(entry).xpath(".//startdate/text()")[0]
+    return _get_time(date)
 
 
-def airdate(episode):
+def _airdate(episode):
     try:
         date = episode.xpath(".//airdate/text()")[0]
     except IndexError:
         return None
-    return get_time(date)
+    return _get_time(date)
 
 
-def related(entry, types=None, incl_type=False):
+def _related(entry, types=None, incl_type=False):
     Anidb.log.debug("Finding shows related to %s" % entry.title)
 
     if types:
         type_str = "[" + " or ".join(['@type="%s"' % t for t in types]) + "]"
     else:
         type_str = ""
-    rel = info(entry).xpath("//relatedanime/anime%s" % type_str)
+    rel = _info(entry).xpath("//relatedanime/anime%s" % type_str)
     result = []
     for r in rel:
         try:
@@ -163,7 +164,7 @@ def related(entry, types=None, incl_type=False):
     return result
 
 
-def family(entries, criteria=lambda e: True, types=None):
+def _family(entries, criteria=lambda e: True, types=None):
     Anidb.log.debug("Getting family of %s" % [e.title for e in entries])
 
     relations = []
@@ -179,34 +180,34 @@ def family(entries, criteria=lambda e: True, types=None):
                 relations.append(entry)
             # continue searching if a direct pr/sequel or meets criteria
             if rel_type in ['Sequel', 'Prequel'] or criteria(entry, rel_type):
-                queue += related(entry, types, True)
+                queue += _related(entry, types, True)
 
     Anidb.log.debug("Family found for %s" % [e.title for e in entries])
     return relations
 
 
-def episodes(entry):
-    eps = info(entry).xpath('.//episodes/episode/epno[@type="1"]/text()')
+def _episodes(entry):
+    eps = _info(entry).xpath('.//episodes/episode/epno[@type="1"]/text()')
     return sorted(map(int, eps))
 
 
 @filecache(7 * 24 * 60 * 60)
-def find_seasons(entry):
+def _find_seasons(entry):
     Anidb.log.debug("Getting seasons for %s" % entry.title)
 
     def is_season(e, rel_type):
         if e == entry:
             return True
-        e_type = info(e).xpath("//type/text()")[0]
+        e_type = _info(e).xpath("//type/text()")[0]
         is_series = e_type == "TV Series"
         is_movie = e_type == "Movie"
         is_special = e_type == "TV Special"
         is_sequel = not rel_type or rel_type == "Sequel"
         return (is_series or is_sequel) and not (is_movie or is_special)
-    seasons = family([entry], is_season)
+    seasons = _family([entry], is_season)
 
     # sort series by start date for correct season numbers
-    seasons = sorted(seasons, key=startdate)
+    seasons = sorted(seasons, key=_startdate)
 
     # eliminate entries earlier than requested season
     entry_index = seasons.index(entry)
@@ -216,30 +217,30 @@ def find_seasons(entry):
     return seasons[entry_index:]
 
 
-def find_specials(entry):
+def _find_specials(entry):
     # first search seasons for special episodes
-    season_entries = find_seasons(entry)
+    season_entries = _find_seasons(entry)
     lang_str = " or ".join(['@*="%s"' % la for la in langs])
     titles_path = './title[%s]/text()' % lang_str
     specials = []
     for series in season_entries:
         # pick special episodes from regular seasons
-        eps = info(series).xpath(
+        eps = _info(series).xpath(
             './/episodes/episode/epno[@type="2"]/..')
         for ep in eps:
-            if airdate(ep) is None or airdate(ep) <= time.time():
-                specials.append((ep.xpath(titles_path), airdate(ep)))
+            if _airdate(ep) is None or _airdate(ep) <= time.time():
+                specials.append((ep.xpath(titles_path), _airdate(ep)))
 
     # next search special series for episodes (e.g. spinoffs)
-    sp_series = chain(*[related(e) for e in season_entries])
+    sp_series = chain(*[_related(e) for e in season_entries])
     sp_series = filter(lambda s: s not in season_entries, sp_series)
 
     for series in sp_series:
         # pick regular and special episodes from special seasons
-        eps = info(series).xpath(
+        eps = _info(series).xpath(
             './/episodes/episode/epno[@type="1" or @type="2"]/..')
         for ep in eps:
-            if airdate(ep) is None or airdate(ep) <= time.time():
+            if _airdate(ep) is None or _airdate(ep) <= time.time():
                 if len(eps) == 1:
                     # one episode means series title is real title
                     titles = series.titles
@@ -248,12 +249,12 @@ def find_specials(entry):
                     # combine episode titles with season titles
                     combs = product(series.titles, titles)
                     titles.extend([t1 + " " + t2 for t1, t2 in combs])
-                specials.append((titles, airdate(ep)))
+                specials.append((titles, _airdate(ep)))
 
     return specials
 
 
-def similarity(show, ep, titles):
+def _similarity(show, ep, titles):
     text_comp = max([comp(ep.title, t, show.titles)
                      for t in titles[0]])**2
     month = 60 * 60 * 24 * 30
@@ -266,12 +267,12 @@ def similarity(show, ep, titles):
 m = Munkres()
 
 
-def pair_episodes_to_titles(show, episodes, title_set):
+def _pair_episodes_to_titles(show, episodes, title_set):
     ep_nums = list(enumerate(episodes))
     title_nums = list(enumerate(title_set))
 
     # build matrix out of similarities
-    cost_matrix = [[1.0 - similarity(show, ep, t) for tn, t in title_nums]
+    cost_matrix = [[1.0 - _similarity(show, ep, t) for tn, t in title_nums]
                    for epn, ep in ep_nums]
 
     # Hungarian algorithm
@@ -285,47 +286,51 @@ def pair_episodes_to_titles(show, episodes, title_set):
 
 class Anidb(Database):
 
+    """ Plugin that accesses show data from anidb. """
+
     def __init__(self):
+        """ Create anidb plugin. """
         if not _init_flag:
             _init()
 
-    def show_get(self, show):
+    def _show_get(self, show):
         # add data about other titles for this show
         Anidb.log.debug("Getting data for %s" % show)
         try:
-            return entries(show.title)[0]
+            return _entries(show.title)[0]
         except IndexError:
             Anidb.log.debug("Couldn't find %s" % show)
             return None
 
     def get_show_titles(self, show):
-        # add data about other titles for this show
-        data = self.show_get(show)
+        """ Add data about other titles for this show. """
+        data = self._show_get(show)
         if data is None:
             return []
         return data.titles
 
     def get_show_absolute_numbering(self, show):
-        # if on anidb, could use absolute episode numbering
-        if self.show_get(show) is not None:
+        """ If this show is on anidb, return true. """
+        if self._show_get(show) is not None:
             return True
         else:
             return False
 
     def get_seasons(self, show):
-        data = self.show_get(show)
+        """ Get additional titles for show seasons. """
+        data = self._show_get(show)
         if data is None:
             return []
 
-        seasons = find_seasons(data)
+        seasons = _find_seasons(data)
         return [Season(show, num, se.title, se.titles)
                 for num, se in enumerate(seasons, 1)]
 
     # OBSOLETE
-    def add_data(self, sh):
+    def _add_data(self, sh):
         # add data about other titles for this show
         try:
-            match = entries(sh.title)[0]
+            match = _entries(sh.title)[0]
         except IndexError:
             Anidb.log.debug("Couldn't find %s" % sh)
             return
@@ -335,7 +340,7 @@ class Anidb(Database):
         sh.absolute = True
 
         # find additional seasons
-        season_entries = find_seasons(match)
+        season_entries = _find_seasons(match)
 
         # add titles for seasons
         season_sp = None
@@ -353,18 +358,18 @@ class Anidb(Database):
             return
 
         # find specials relating to this show
-        specials = find_specials(match)
+        specials = _find_specials(match)
 
         if len(specials) == 0:
             # no special episodes found
             return
 
         # match wanted specials to find special titles
-        assign = pair_episodes_to_titles(sh, season_sp.itervalues(), specials)
+        assign = _pair_episodes_to_titles(sh, season_sp.itervalues(), specials)
         for ep in assign:
             titles, airdate = assign[ep]
             # root out any obvious non-matches
-            if similarity(sh, ep, assign[ep]) < 0.10:
+            if _similarity(sh, ep, assign[ep]) < 0.10:
                 continue
             ep.titles.update(titles)
 
